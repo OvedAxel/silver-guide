cfb2a2a43db78e485908f159b134cd102c570537
OvedAxel <62026969+OvedAxel@users.noreply.github.com>
2020-09-14 20:22:19 -0500

    Add files via upload

---
+++ Actividad 5/src/com/Main.java
@@ -0,0 +1,19 @@
+package com;
+
+public class Main {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int suma = a+b;
+        int resta = a-b;
+        int mult = a*b;
+        int div = a/b;
+        int mod = a%b;
+
+        System.out.println("La suma es: " +suma);
+        System.out.println("La resta es: " +resta);
+        System.out.println("La multiplicación es: " +mult);
+        System.out.println("La división es: " +div);
+        System.out.println("El residuo es: " +mod);
+    }
+}
